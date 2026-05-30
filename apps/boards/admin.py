6da67348271c45f3021832a557b6ae6c2e9f854d
lucaslezcano03 <lucaslezcano0305@gmail.com>
2026-05-30 09:54:15 -0300

    feat: agregar modelos de tableros y columnas

--- apps/boards/admin.py
+++ apps/boards/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Board, BoardColumn
 
-# Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'creado_por', 'fecha_creacion')
+    search_fields = ('nombre', 'descripcion')
+
+
+@admin.register(BoardColumn)
+class BoardColumnAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'tablero', 'posicion')
+    list_filter = ('tablero',)
